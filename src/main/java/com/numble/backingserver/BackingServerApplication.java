c009ef750a5af8c9f27ef595b9c12f3dcd497fab
Yiseull <omjl5123@catholic.ac.kr>
2023-02-18 15:31:17 +0900

    Initial setting

---
+++ src/main/java/com/numble/backingserver/BackingServerApplication.java
@@ -0,0 +1,13 @@
+package com.numble.backingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numble/backingserver/BackingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.backingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
