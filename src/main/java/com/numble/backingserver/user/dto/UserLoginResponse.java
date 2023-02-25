d3612c3f7540bef44e371590a02c2e0dfe615b27
Yiseull <omjl5123@catholic.ac.kr>
2023-02-25 23:35:51 +0900

    Fix error that response cannot be generated with json

--- src/main/java/com/numble/backingserver/user/dto/UserLoginResponse.java
+++ src/main/java/com/numble/backingserver/user/dto/UserLoginResponse.java
@@ -1,11 +1,10 @@
 package com.numble.backingserver.user.dto;
 
-import com.numble.backingserver.user.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.*;
 
 @AllArgsConstructor
 @Builder
+@Getter
 public class UserLoginResponse {
 
     private int userId;
@@ -13,10 +12,4 @@ public class UserLoginResponse {
     private String name;
     private String phoneNumber;
 
-    public void toDto(User user) {
-        userId = user.getUserId();
-        email = user.getEmail();
-        name = user.getName();
-        phoneNumber = user.getPhoneNumber();
-    }
 }
