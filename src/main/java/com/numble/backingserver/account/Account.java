69c60803f07b0edfe269929ef13bb33f5d047c1d
Yiseull <omjl5123@catholic.ac.kr>
2023-02-26 01:05:19 +0900

    Add account entity

---
+++ src/main/java/com/numble/backingserver/account/Account.java
@@ -0,0 +1,33 @@
+package com.numble.backingserver.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@DynamicInsert
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "account")
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private int accountId;
+    @Column(name = "account_number", nullable = false)
+    private int accountNumber;
+    @Column(nullable = false)
+    private int pin;
+    @Column(nullable = false)
+    private int balance;
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+}
